88ebc384a17ef14f37fadf48bf433c115d631942
noaione <noaione0809@gmail.com>
2022-06-05 15:48:01 +0800

    Use Inter font, same as the original website

--- app/src/main/java/me/naoti/panelapp/ui/theme/Type.kt
+++ app/src/main/java/me/naoti/panelapp/ui/theme/Type.kt
@@ -1,13 +1,28 @@
 package me.naoti.panelapp.ui.theme
 
+import me.naoti.panelapp.R
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+val InterFamily = FontFamily(
+    Font(R.font.inter_thin, FontWeight.Thin),
+    Font(R.font.inter_extra_light, FontWeight.ExtraLight),
+    Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.inter_black, FontWeight.Black)
+)
+
 // Set of Material typography styles to start with
 val Typography = Typography(
+    defaultFontFamily = InterFamily,
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
